c3827c7ca45c421d5a8181e79a0b347f527864fc
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-06-19 15:58:31 +0200

    CheCluster now uses custom Che Devfile Registry.

--- provision-optaplanner-workshop.sh
+++ provision-optaplanner-workshop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROJECT_NAME="optaplanner-workshop"
+PROJECT_NAME="optaplanner-workshop-2"
 
 echo "Creating project: $PROJECT_NAME"
 oc new-project $PROJECT_NAME --display-name="OptaPlanner Workshop" --description="OptaPlanner Workshop"
@@ -11,6 +11,10 @@ oc new-project $PROJECT_NAME --display-name="OptaPlanner Workshop" --description
 echo "Installing CodeReady Workspaces Operator."
 cat optaplanner-workshop-operator-group.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc apply -n $PROJECT_NAME -f -
 cat codeready-workspaces-operator-sub.yaml | sed -e "s/namespace-placeholder/$PROJECT_NAME/g" | oc apply -n $PROJECT_NAME -f - 
+
+# This seems to be needed to allow the API of Che resources/CRDs to be available in the cluster.
+echo "Wait for the Operator to be installed."
+sleep 10
 
 # Create Che Cluster
 echo "Creating CodeReady Workspaces cluster."
